42bd74f3f9183b5f5beb4781f019d31770182ea7
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-04-16 23:06:53 +0100

    create routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AddEmployee from './components/AddEmployee';
 import Navbar from './components/Navbar';
+import EmployeeList from './components/EmployeeList';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Navbar />
-      <AddEmployee />
+      <Routes>
+        <Route index element={<EmployeeList />} />
+        <Route path='/' element={<EmployeeList/>}/>
+        <Route path='/employeeList' element={<EmployeeList/>} />
+        <Route path='/addEmployee' element={<AddEmployee />}/>
+      </Routes>
+    </BrowserRouter>
+      
     </>
   );
 }
