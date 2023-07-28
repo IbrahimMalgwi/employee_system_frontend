9e3d6f368de0393d608c9bafa1196c9faffec395
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-07-28 05:44:04 +0100

    update

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -41,7 +41,7 @@ const AddEmployee = () => {
 
   return (
     <div className='flex max-w-2xl mx-auto shadow border-b'>
-      <div className='px-9 py-8'>
+      <div className='px-8 py-8'>
          <div className='font-thin text-2xl tracking-wider'>
             <h1>Add new Employee</h1>
          </div>
