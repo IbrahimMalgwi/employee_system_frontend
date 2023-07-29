e0a1d949a5442b0e3297c7cfdf6f45ff13d09c24
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-07-29 18:39:06 +0100

    refactored

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import EmployeeService from '../services/EmployeeService';
 import { useNavigate } from 'react-router-dom';
 
